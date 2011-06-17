f45567a5b1f690dbcb99d381c00d1da15480dc01
Michael Hoitomt <mike.hoitomt@gmail.com>
2011-06-16 23:06:54 -0500

    New App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HoitomtCom::Application.config.secret_token = 'a0fa3ee9a168530297103bd6a3ccda8af30d678f2a564a9e6c87cb0b83de8041abc9f99cf976cada3bd32f0f0fb4379c31ba3a7ecc504f561b6933b05ce14480'
