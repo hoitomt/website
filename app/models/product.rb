22520c6c0e75ae10147087d5e087518f89a7fbad
Michael Hoitomt <mike.hoitomt@gmail.com>
2011-07-14 17:06:41 -0500

    Update Jquery and add reference to repair delete

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ActiveRecord::Base
   has_attached_file :photo, 
                     :styles => { :medium => "300x300>" },
      :storage => :s3,
-     :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+     :s3_credentials => "#{Rails.root.to_s}/config/s3.yml",
      :path => "/:id/:filename"
 end
