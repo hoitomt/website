f77f0c180bf8c42cd0e44b6eecec3482120ddad9
Michael Hoitomt <mike.hoitomt@gmail.com>
2012-01-20 15:03:28 -0600

    fucking with pdf

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 
   def about
     @title = "About Me"
+    send_pdf_file(about.pdf) if Rails.env.production?
   end
 
   def projects
@@ -10,6 +11,12 @@ class PagesController < ApplicationController
 
   def resume
     @title = "Resume"
+    send_pdf_file(resume.pdf) if Rails.env.production?
   end
-
+  
+  def send_pdf_file(file_name)
+    send_file("#{Rails.root}/public/documents/#{file_name}", 
+              :disposition => 'inline', 
+              :type => 'application/pdf')
+  end
 end
